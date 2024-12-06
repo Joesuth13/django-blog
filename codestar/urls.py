30f3f9511c96262e235645107468bc597054f0d9
Samuel Sutherland <joesuth13@gmail.com>
2024-12-06 12:52:45 +0000

    update home with generic post list

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import hello_world 
+
 
 urlpatterns = [
-    path('hello/', hello_world, name='blog'),
+    path("", include("blog.urls"), name='blog-urls'),
     path('admin/', admin.site.urls),
 ]
