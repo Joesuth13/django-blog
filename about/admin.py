36698bbb9df0b78ede6952ac4b8286ba76ce0932
Samuel Sutherland <joesuth13@gmail.com>
2024-12-11 14:35:32 +0000

    Wrap up and validate

--- about/admin.py
+++ about/admin.py
@@ -5,7 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
+    """
     
+    """
     summernote_fields = ('content',)
 
 
--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,6 @@
-from . import views
-from django.urls import path
 
+from django.urls import path
+from . import views
 urlpatterns = [
 
     path('', views.AboutView, name='about'),
